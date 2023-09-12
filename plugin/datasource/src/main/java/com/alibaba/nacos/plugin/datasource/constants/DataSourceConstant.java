1310587a25c6a5d17d6c80891d5497dc90a17c08
taodizhou <taodizhou@wisecotech.com>
2023-09-12 19:16:59 +0800

    update config setting example

--- plugin/datasource/src/main/java/com/alibaba/nacos/plugin/datasource/constants/DataSourceConstant.java
+++ plugin/datasource/src/main/java/com/alibaba/nacos/plugin/datasource/constants/DataSourceConstant.java
@@ -26,5 +26,6 @@ public class DataSourceConstant {
     public static final String MYSQL = "mysql";
     
     public static final String DERBY = "derby";
+
     public static final String POSTGRESQL = "postgresql";
 }
